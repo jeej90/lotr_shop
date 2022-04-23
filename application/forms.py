4bb6ae91ef9fe359c880b8fa873897d4a7c2ad25
Jeje <jehan.alfarra@gmail.com>
2022-04-23 09:53:53 +0100

    length for email field updated

--- application/forms.py
+++ application/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from application.models import RegisteredUser
 
 
 # JeJe: added reg & login forms
 class RegistrationForm(FlaskForm):
     user_name = StringField('User name', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=100)])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
